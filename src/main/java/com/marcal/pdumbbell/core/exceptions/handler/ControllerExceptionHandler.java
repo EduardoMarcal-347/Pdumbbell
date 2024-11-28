f2240f53b53af055b7c2a23ad4e830567dd667ac
Eduardo Marçal <edumsfilho347@gmail.com>
2024-11-27 23:35:08 -0300

    feature: creating exception handler for internal errors at incoming requests

---
+++ src/main/java/com/marcal/pdumbbell/core/exceptions/handler/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.marcal.pdumbbell.core.exceptions.handler;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Log4j2
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGlobalExceptions(Exception e) {
+        Map<String, String> errorResponse = new HashMap<>();
+        String userFriendlyMessage = "An unexpected error has occurred. Please try again later.";
+        errorResponse.put("error", userFriendlyMessage);
+        log.error("Error during API request", e);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
