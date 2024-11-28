e514905e9c65881dba3cd923b57ffdcf5f480233
Eduardo Marçal <edumsfilho347@gmail.com>
2024-11-27 23:33:46 -0300

    chore: creating first project skeleton

---
+++ src/test/java/com/marcal/pdumbbell/PdumbbellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcal.pdumbbell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdumbbellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
