259468ccc4c85180d6775cd89eacdbcb67058ead
Eduardo Marçal <edumsfilho347@gmail.com>
2024-12-10 00:35:37 -0300

    feat: creating new responseUtil to api responses

--- src/main/java/com/marcal/pdumbbell/exceptions/handler/ControllerExceptionHandler.java
+++ src/main/java/com/marcal/pdumbbell/exceptions/handler/ControllerExceptionHandler.java
@@ -1,35 +1,54 @@
 package com.marcal.pdumbbell.exceptions.handler;
 
+import com.marcal.pdumbbell.dto.shared.ErrorResponseDTO;
+import com.marcal.pdumbbell.utils.ResponseUtil;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.AccessDeniedException;
 
 @Log4j2
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage())
-        );
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler( MethodArgumentNotValidException.class )
+    public ResponseEntity<ErrorResponseDTO> handleValidationExceptions( MethodArgumentNotValidException ex ) {
+        return ResponseEntity
+                .status( HttpStatus.BAD_REQUEST )
+                .body( ResponseUtil.errorResponse( 400,
+                        ex.getBindingResult( ).getFieldErrors( ).stream( )
+                                .findFirst( )
+                                .map( DefaultMessageSourceResolvable::getDefaultMessage )
+                                .orElse( null )
+                ) );
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGlobalExceptions(Exception e) {
-        Map<String, String> errorResponse = new HashMap<>();
-        String userFriendlyMessage = "An unexpected error has occurred. Please try again later.";
-        errorResponse.put("error", userFriendlyMessage);
-        log.error("Error during API request", e);
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler( AuthenticationServiceException.class )
+    public ResponseEntity<ErrorResponseDTO> handleUnauthorizedError( AuthenticationServiceException ex ) {
+        return ResponseEntity
+                .status( HttpStatus.UNAUTHORIZED )
+                .body( ResponseUtil.unauthorizedError( ) );
+    }
+
+    @ExceptionHandler( AccessDeniedException.class )
+    public ResponseEntity<ErrorResponseDTO> handleForbiddenError( AccessDeniedException ex ) {
+        return ResponseEntity
+                .status( HttpStatus.FORBIDDEN )
+                .body( ResponseUtil.forbiddenError( ) );
+    }
+
+    @ExceptionHandler( Exception.class )
+    public ResponseEntity<ErrorResponseDTO> handleGlobalExceptions( Exception e ) {
+        log.error( "Error during API request", e );
+        return ResponseEntity
+                .status( HttpStatus.INTERNAL_SERVER_ERROR )
+                .body( ResponseUtil.internalServerError( ) );
     }
 
 }
--- src/main/java/com/marcal/pdumbbell/utils/ResponseUtil.java
+++ src/main/java/com/marcal/pdumbbell/utils/ResponseUtil.java
@@ -8,15 +8,6 @@ import java.util.Optional;
 
 public class ResponseUtil {
 
-    public static SuccessResponseDTO<?> successResponse( Object data ) {
-        return new SuccessResponseDTO<>(
-                200,
-                "The request was successfully completed!",
-                Instant.now( ),
-                data
-        );
-    }
-
     public static SuccessResponseDTO<?> successResponse( Object data, String message ) {
         return new SuccessResponseDTO<>(
                 200,
@@ -42,6 +33,34 @@ public class ResponseUtil {
         return errorResponse(
                 33,
                 "This " + field + " is already registered by another " + schema + "!"
+        );
+    }
+
+    public static ErrorResponseDTO internalServerError( ) {
+        return errorResponse(
+                500,
+                "An unexpected error has occurred. Please try again later."
+        );
+    }
+
+    public static ErrorResponseDTO forbiddenError( ) {
+        return errorResponse(
+                403,
+                "Forbidden: Access is denied"
+        );
+    }
+
+    public static ErrorResponseDTO unauthorizedError( ) {
+        return errorResponse(
+                401,
+                "Unauthorized: Authentication failed"
+        );
+    }
+
+    public static ErrorResponseDTO unexistentLoginIdentifierError( ) {
+        return errorResponse(
+                400,
+                "Unexistent user registered with this username/email"
         );
     }
 
