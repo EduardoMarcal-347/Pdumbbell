82b916fbc031985d1e13be0f899d601d17a19d00
Eduardo Marçal <edumsfilho347@gmail.com>
2024-12-12 01:47:55 -0300

    feat: initial logic to user initial page endpoint

--- src/main/java/com/marcal/pdumbbell/controllers/UserController.java
+++ src/main/java/com/marcal/pdumbbell/controllers/UserController.java
@@ -1,34 +1,29 @@
 package com.marcal.pdumbbell.controllers;
 
 
+import com.marcal.pdumbbell.dto.shared.BaseResponse;
 import com.marcal.pdumbbell.services.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
-@RequestMapping("/pdumbbell/api/user")
+@RequestMapping( "/pdumbbell/api/user" )
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController( UserService userService ) {
         this.userService = userService;
     }
 
-//    @GetMapping("/id/{id}")
-//    public ResponseEntity<?> getById(@PathVariable String id) {
-//        return userService.getById(id);
-//    }
-//
-//    @PutMapping("/id/{id}")
-//    public ResponseEntity<?> update(@PathVariable String id, @Valid @RequestBody UserDto dto) {
-//        return userService.update(id, dto);
-//    }
-//
-//    @DeleteMapping("/id/{id}")
-//    public ResponseEntity<?> delete(@PathVariable String id) {
-//        return userService.delete(id);
-//    }
+    @GetMapping( "/me" )
+    public ResponseEntity<BaseResponse> getByToken( @RequestHeader( value = "Authorization" ) String token ) {
+        return userService.findUserByToken( token );
+    }
+
 }
--- src/main/java/com/marcal/pdumbbell/services/UserService.java
+++ src/main/java/com/marcal/pdumbbell/services/UserService.java
@@ -1,16 +1,35 @@
 package com.marcal.pdumbbell.services;
 
+import com.marcal.pdumbbell.dto.mappers.response.UserResponseMapper;
+import com.marcal.pdumbbell.dto.response.UserResponseDTO;
+import com.marcal.pdumbbell.dto.shared.BaseResponse;
+import com.marcal.pdumbbell.entities.domain.User;
 import com.marcal.pdumbbell.repositories.UserRepository;
-import com.marcal.pdumbbell.security.service.PasswordService;
+import com.marcal.pdumbbell.utils.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final AuthService authService;
+
     private final UserRepository userRepository;
 
-    public UserService( UserRepository userRepository, PasswordService passwordService ) {
+    public UserService( AuthService authService, UserRepository userRepository ) {
+        this.authService = authService;
         this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<BaseResponse> findUserByToken( String token ) {
+        User user = authService.loadUserByToken( token ).orElse( null );
+        if ( user == null ) {
+            return ResponseEntity.status( HttpStatus.UNAUTHORIZED )
+                    .body( ResponseUtil.invalidTokenError( ) );
+        }
+        UserResponseDTO responseDTO = UserResponseMapper.INSTANCE.toDto( userRepository.save( user ) );
+        return ResponseEntity.ok( ResponseUtil.successResponse( responseDTO, null ) );
     }
 
 
